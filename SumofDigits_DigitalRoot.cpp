e67305d6fb8a53234f90c28330e12a27d53b39e4
halfwaytonowhere <86977264+halfwaytonowhere@users.noreply.github.com>
2021-12-14 15:39:04 +0100

    Add files via upload

---
+++ SumofDigits_DigitalRoot.cpp
@@ -0,0 +1,15 @@
+﻿int digital_root(int n)
+{
+    int sumacyfr = 0;
+    while (n > 0)
+    {
+        sumacyfr += n % 10;
+        n /= 10;
+    }
+
+    if (sumacyfr > 9)
+        return digital_root(sumacyfr);
+    else
+        return sumacyfr;
+
+}
